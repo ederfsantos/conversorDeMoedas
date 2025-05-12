0b773cc9a7df78c4ff5a8aade4ddee5c7fb28602
Eder Ferreira Santos <ederfsantos@live.com>
2025-05-12 18:04:48 -0300

    criando a classe de conversao de valores com o metodos

---
+++ src/modelo/ConversaoDeValores.java
@@ -0,0 +1,34 @@
+package modelo;
+
+public class ConversaoDeValores {
+
+    public Double converteDolarParaPesoArgentino(Double valor){
+
+        return null;
+
+    }
+    public Double convertePesoArgentinoParaDolar(Double valor){
+
+        return null;
+
+    }
+    public Double converteDolarParaRealBrasileiro(Double valor) {
+
+        return null;
+    }
+    public Double converteRealBrasileiroParaDolar(Double valor) {
+
+        return null;
+    }
+
+    public Double converteDolarParaPesoColombiano(Double valor) {
+
+        return null;
+    }
+
+    public Double convertePesoColombianoParaDolar(Double valor) {
+
+        return null;
+    }
+
+}
--- src/modelo/Principal.java
+++ src/modelo/Principal.java
@@ -1,14 +1,36 @@
 package modelo;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         int opcao;
+
+        Scanner ler = new Scanner(System.in);
         System.out.println("*********************************************************************");
         System.out.println("Seja bem-vindo/a ao Conversor de Moedas!\n");
-        System.out.println("1 - Dólar para Peso Argentino\n2 - Peso Argentino para Dólar\n" +
-                           "3 - Dólar para Real Brasileiro\n4 - Real Brasileiro para Dólar\n" +
-                           "5 - Dólar para Peso Colombiano\n6 - Peso Colombiano para Dólar\n" +
-                           "7 - Sair da aplicação\nEscolha uma opção válida:");
-        System.out.println("*********************************************************************");
+
+        do {
+            System.out.println("1 - Dólar para Peso Argentino\n2 - Peso Argentino para Dólar\n" +
+                               "3 - Dólar para Real Brasileiro\n4 - Real Brasileiro para Dólar\n" +
+                               "5 - Dólar para Peso Colombiano\n6 - Peso Colombiano para Dólar\n" +
+                               "7 - Sair da aplicação");
+            System.out.println("Escolha uma opção válida:");
+            opcao = ler.nextInt();
+            System.out.println("*********************************************************************");
+
+            switch (opcao){
+
+
+                case 1:
+                {
+
+                }
+            }
+
+
+        } while (opcao != 7 || opcao > 7);
+
+
     }
 }
