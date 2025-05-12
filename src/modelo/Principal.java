841988534776956649a2b96c37858539f7e8d249
Eder Ferreira Santos <ederfsantos@live.com>
2025-05-12 17:42:14 -0300

    criando o menu de opcoes de moedas

---
+++ src/modelo/Principal.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class Principal {
+    public static void main(String[] args) {
+        int opcao;
+        System.out.println("*********************************************************************");
+        System.out.println("Seja bem-vindo/a ao Conversor de Moedas!\n");
+        System.out.println("1 - Dólar para Peso Argentino\n2 - Peso Argentino para Dólar\n" +
+                           "3 - Dólar para Real Brasileiro\n4 - Real Brasileiro para Dólar\n" +
+                           "5 - Dólar para Peso Colombiano\n6 - Peso Colombiano para Dólar\n" +
+                           "7 - Sair da aplicação\nEscolha uma opção válida:");
+        System.out.println("*********************************************************************");
+    }
+}
